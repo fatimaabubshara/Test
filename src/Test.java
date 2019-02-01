47c58b83cceaeddf378f369c919f34aee32189af
MaramHasan <47158050+MaramHasan@users.noreply.github.com>
2019-02-01 17:13:13 +0200

    New One

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java. awt.*;
+
+public class Test {
+	public static void main(String[] args) {
+		int x =5 ;
+		int y=9;
+		int z =x+y ;
+		System.out.print(z);
+	}
+}
